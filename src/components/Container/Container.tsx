266f05c911801d0db78b375d75002a6ffb3879a2
Sandu Morari <sandu.m.md@gmail.com>
2025-10-23 21:32:19 +0200

    Configured `Container` widths depending on screen width

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -9,7 +9,7 @@ type ContainerProps = {
 
 export default function Container({ children, className, style }: ContainerProps) {
   return (
-    <div className={clsx('max-w-[1200px] mx-auto px-4 sm:px-6', className)}
+    <div className={clsx('max-w-[1200px] mx-auto px-4 sm:px-6 md:max-w-[700px] lg:max-w-[900px] xl:max-w-[1024px] 2xl:max-w-[1440px] w-full', className)}
       style={style}
     // sx={{
     //   px: {
