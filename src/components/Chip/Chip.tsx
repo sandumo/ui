2e34bfb5c6026c9fef96f467e93c0ef2234c7af5
Sandu Morari <sandu.m.md@gmail.com>
2025-10-25 17:58:51 +0200

    `Chip`: support for `size` prop

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,13 +1,25 @@
+import { twMerge } from 'tailwind-merge';
+
 type ChipProps = React.HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
   icon?: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
 };
 
-export default function Chip({ children, className, icon, ...props }: ChipProps) {
+export default function Chip({ children, className, icon, size = 'md', ...props }: ChipProps) {
   return (
-    <div className={`flex bg-gray-100 rounded-full px-3 py-1 border border-gray-200 ${className}`} {...props}>
+    <div
+      className={twMerge(
+        'flex bg-gray-100 rounded-full border border-gray-200',
+        size === 'sm' && 'px-1.5 py-0.5 text-xs',
+        size === 'md' && 'px-3 py-1 text-sm',
+        size === 'lg' && 'px-4 py-2 text-lg',
+        className,
+      )}
+      {...props}
+    >
       {icon && <div className="mr-2 -ml-2">{icon}</div>}
-      <div className="text-sm">{children}</div>
+      <div>{children}</div>
     </div>
   );
 }
