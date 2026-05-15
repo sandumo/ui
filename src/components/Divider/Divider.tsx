47c754854570f82a622f512a45a3775be7c07066
Sandu Morari <sandu.m.md@gmail.com>
2026-05-15 22:32:11 +0200

    `Divider`: fixed className handling

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,4 +1,4 @@
-import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 type DividerProps = {
   label?: string;
@@ -8,14 +8,14 @@ type DividerProps = {
 
 export default function Divider({ label, className, vertical = false }: DividerProps) {
   if (!label) {
-    return <div className={clsx('bg-divider', vertical ? 'w-[1px] h-full' : 'w-full h-[1px]', className)} />;
+    return <div className={twMerge('bg-divider', vertical ? 'w-[1px] h-full' : 'w-full h-[1px]', className)} />;
   }
 
   return (
-    <div className={clsx('flex items-center gap-2', className)}>
-      <div className={clsx('bg-divider flex-1', vertical ? 'w-[1px] h-full' : 'w-full h-[1px]')} />
+    <div className={twMerge('flex items-center gap-2', className)}>
+      <div className={twMerge('bg-divider flex-1', vertical ? 'w-[1px] h-full' : 'w-full h-[1px]')} />
       {label && <div className="text-sm">{label}</div>}
-      <div className={clsx('bg-divider flex-1', vertical ? 'w-[1px] h-full' : 'w-full h-[1px]')} />
+      <div className={twMerge('bg-divider flex-1', vertical ? 'w-[1px] h-full' : 'w-full h-[1px]')} />
     </div>
   );
 }
